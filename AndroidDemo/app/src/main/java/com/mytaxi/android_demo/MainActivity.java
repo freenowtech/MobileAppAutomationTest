20b734da4becdb76ebe46a7bcfcb78a4c5d4f279
Jing Li <j.li@mytaxi.com>
2017-11-24 09:47:41 +0100

    Change lat & lng, update zoom level

--- AndroidDemo/app/src/main/java/com/mytaxi/android_demo/MainActivity.java
+++ AndroidDemo/app/src/main/java/com/mytaxi/android_demo/MainActivity.java
@@ -2,16 +2,15 @@ package com.mytaxi.android_demo;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -94,8 +93,8 @@ public class MainActivity extends AppCompatActivity
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        LatLng officeMytaxi = new LatLng(53.544604, 9.9265683);
+        LatLng officeMytaxi = new LatLng(53.544604, 9.928757);
         mMap.addMarker(new MarkerOptions().position(officeMytaxi).title("mytaxi Office"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(officeMytaxi));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(officeMytaxi, 18.0f));
     }
 }
