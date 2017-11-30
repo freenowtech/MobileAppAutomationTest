c7fb1c9064bb2615f386fb605fc71e04842bdf84
Jing Li <j.li@mytaxi.com>
2017-11-30 11:38:20 +0100

    Customize DriverAdapter

--- AndroidDemo/app/src/main/java/com/mytaxi/android_demo/adapters/DriverAdapter.java
+++ AndroidDemo/app/src/main/java/com/mytaxi/android_demo/adapters/DriverAdapter.java
@@ -3,45 +3,103 @@ package com.mytaxi.android_demo.adapters;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Filter;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.mytaxi.android_demo.models.Driver;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class DriverAdapter extends ArrayAdapter {
-    private Context mContext;
+public class DriverAdapter extends ArrayAdapter<Driver> {
 
-    public DriverAdapter(@NonNull Context context, @NonNull List drivers) {
-        super(context, android.R.layout.select_dialog_item, getDrvierNamesList(drivers));
-        mContext = context;
+    private List<Driver> mDrivers;
+    private final List<Driver> mDriversCompleteSet;
+    private LayoutInflater mLayoutInflater;
+    private OnClickListener mOnClickListener;
+    private static final int sResource = android.R.layout.select_dialog_item;
+
+    public DriverAdapter(@NonNull Context context, List<Driver> drivers, OnClickListener onClickListener) {
+        super(context, sResource, drivers);
+        mDrivers = new ArrayList<>(drivers);
+        mDriversCompleteSet = new ArrayList<>(drivers);
+        mLayoutInflater = LayoutInflater.from(context);
+        mOnClickListener = onClickListener;
+    }
+
+    @Override
+    public int getCount() {
+        return mDrivers.size();
+    }
+
+    @Override
+    public Driver getItem(int i) {
+        return mDrivers.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
     }
 
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        View view = super.getView(position, convertView, parent);
-        view.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                TextView textView = (TextView) view.findViewById(android.R.id.text1);
-                String name = textView.getText().toString();
-                Toast.makeText(mContext, "Calling: " + name, Toast.LENGTH_SHORT).show();
-            }
-        });
-        return view;
+        if (convertView == null) {
+            convertView = mLayoutInflater.inflate(sResource, parent, false);
+        }
+        convertView.setOnClickListener(mOnClickListener);
+        TextView textView = convertView.findViewById(android.R.id.text1);
+        textView.setText(mDrivers.get(position).getName());
+        return convertView;
     }
 
-    private static ArrayList<String> getDrvierNamesList(List<Driver> drivers) {
-        ArrayList<String> driverNames = new ArrayList<>();
-        for (Driver driver : drivers) {
-            driverNames.add(driver.getName());
-        }
-        return driverNames;
+    @NonNull
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            public CharSequence convertResultToString(Object resultValue) {
+                return ((Driver) resultValue).getName();
+            }
+
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                FilterResults filterResults = new FilterResults();
+                ArrayList<Driver> suggestions = new ArrayList<>();
+                if (charSequence != null) {
+                    for (Driver driver : mDriversCompleteSet) {
+                        if (driver.getName().toLowerCase().startsWith(charSequence.toString().toLowerCase())) {
+                            suggestions.add(driver);
+                        }
+                    }
+                }
+                filterResults.values = suggestions;
+                filterResults.count = suggestions.size();
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                mDrivers.clear();
+                if (filterResults != null && filterResults.count > 0) {
+                    for (Object result : (List<?>) filterResults.values) {
+                        if (result instanceof Driver) {
+                            mDrivers.add((Driver) result);
+                        }
+                    }
+                    notifyDataSetChanged();
+                } else {
+                    mDrivers.addAll(mDriversCompleteSet);
+                    notifyDataSetInvalidated();
+                }
+            }
+        };
     }
+
 }
